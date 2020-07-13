bf3f0be417e1b348a042850be26d70967477b8fa
dfejza <dfejza@bloomberg.net>
2020-07-12 21:08:55 -0400

    very basic backend to service API requests of what the final product should look like. Can proceed with frontend formatting of data

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let rawdata_JMDICT = fs.readFileSync('./assets/JMdict_e.json');
+let JMDICT = JSON.parse(rawdata_JMDICT);
+
+app.post('/getTerm', (req, res) => {
+    const term = req.body.term;
+    var arrayFound = JMDICT.words.filter(function(item) {
+        for(let i = 0; i < item.kanji.length; i++){
+            if(item.kanji[i].text === term) {
+                console.log(item.kanji[i].text);
+                return true;
+            }
+        };
+        return false;
+    });
+    
+    console.log(arrayFound);
+    return res.send({res : arrayFound});
+});
+
+
+app.listen(3001, () =>
+  console.log(`Example app listening on port 3001!`),
+);
